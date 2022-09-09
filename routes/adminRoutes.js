4601e7b0a18144597c4aa801646515390cb68c06
Mechu Torrendell <mechutorrendell@Mechus-MacBook-Air.local>
2022-09-09 15:27:48 -0300

    detalle

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,24 +2,21 @@ const express = require("express");
 const adminRouter = express.Router();
 
 // //USER---------------------------------------------------------
-// //ADMIN
+
 // adminRouter.get("/users", userControll.show);
 
 // //ORDER---------------------------------------------------------
-// //ADMIN
+
 // adminRouter.delete("/orders/:id", orderControll.destroy);
 
-// //ADMIN
 // adminRouter.get("/orders", orderControll.show);
 
 // //PRODUCTS---------------------------------------------------------
-// //ADMIN
 // adminRouter.get("/product/:id", productControll.store);
 
-// //ADMIN
 // adminRouter.delete("/product/:id", productControll.destroy);
 
-// //ADMIN
 // adminRouter.patch("/product/:id", productControll.update);
+
 
 // module.exports = adminRouter;
