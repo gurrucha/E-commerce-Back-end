711b8feb8fb1b4afa8e06ea9f1c8742720bad6d4
sdehl <sjdehlr@gmail.com>
2022-09-13 16:16:48 -0300

    login route admin and token

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,18 +4,11 @@ const jwt = require("jsonwebtoken");
 
 //log a user/admin
 async function login(req, res) {
-  const admin = await User.findOne({
-    $or: [
-      { email: req.body.emailorUsername === "admin@admin.com" },
-      { username: req.body.emailorUsername === "admin" },
-    ],
-  });
-
   const user = await User.findOne({
     $or: [{ email: req.body.emailorUsername }, { username: req.body.emailorUsername }],
   });
 
-  if (admin) {
+  if (user.isAdmin) {
     const compare = await bcrypt.compare(req.body.password, user.password);
 
     if (compare) {
@@ -23,23 +16,21 @@ async function login(req, res) {
         { user: user.username, id: user.id, isAdmin: true },
         process.env.JWT_TOKEN_KEY,
       );
-      res.status(200).json({ token });
+
+      return res.status(200).json({ token, isAdmin: true });
     } else {
-      res.status(400).json({ message: "Invalid credentials." });
+      return res.status(400).json({ message: "Invalid credentials." });
     }
   } else if (user) {
     const compare = await bcrypt.compare(req.body.password, user.password);
     if (compare) {
-      const token = jwt.sign(
-        { user: user.username, id: user.id, isAdmin: false },
-        process.env.JWT_TOKEN_KEY,
-      );
+      const token = jwt.sign({ user: user.username, id: user.id }, process.env.JWT_TOKEN_KEY);
       res.status(200).json({ token });
     } else {
-      res.status(400).json({ message: "Invalid credentials." });
+      return res.status(400).json({ message: "Invalid credentials." });
     }
   } else {
-    res.status(400).json({ message: "Invalid credentials." });
+    return res.status(400).json({ message: "Invalid credentials." });
   }
 }
 
@@ -48,7 +39,7 @@ async function store(req, res) {
   const user = await User.findOne({ email: req.body.email });
 
   if (user) {
-    res.status(409).json({ message: "User already exists." });
+    return res.status(409).json({ message: "User already exists." });
   } else {
     const newUser = new User({
       email: req.body.email,
@@ -56,9 +47,9 @@ async function store(req, res) {
     });
     newUser.save((error) => {
       if (error) {
-        res.status(400).json({ message: "A field is missing." });
+        return res.status(400).json({ message: "A field is missing." });
       } else {
-        res.status(201).json({ message: "New user created." });
+        return res.status(201).json({ message: "New user created." });
       }
     });
   }
