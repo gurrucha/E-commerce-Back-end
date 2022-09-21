9295de7f5f3fab6833cfe07d6e493b1c1055b8ea
sdehl <sjdehlr@gmail.com>
2022-09-21 11:27:59 -0300

    formidable supabase

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,9 @@
 const { Product } = require("../models");
 const Category = require("../models/Category");
-
+const formidable = require("formidable");
+const path = require("path");
+const fs = require("fs");
+const { createClient } = require("@supabase/supabase-js");
 const _ = require("lodash");
 const slugify = require("slugify");
 
@@ -41,39 +44,57 @@ async function show(req, res) {
     }
     return res.json("El producto no existe! Ha sido eliminado");
   } catch (error) {
-    res.status(404)
+    res.status(404);
   }
 }
 
 // Add a new product (only admin)
+
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_API_KEY);
+
 async function store(req, res) {
-  try {
-    const product = await Product.findOne({ name: req.body.product.name });
-    if (product) {
-      return res.json(409);
-    } else {
-      try {
-        const newProduct = new Product({
-          name: req.body.product.name,
-          price: req.body.product.price,
-          stock: req.body.product.stock,
-          description: req.body.product.description,
-          pictures: [],
-          category: req.body.product.category,
-          slug: slugify(req.body.product.name, { lower: true }),
-        });
-        newProduct.save();
-        return res.status(200).json({ message: "El producto se guardo correctamente" });
-      } catch (error) {
-        return res.status(500).json({ message: "Error!" });
+  const form = formidable({
+    multiples: true,
+    keepExtensions: true,
+  });
+  form.parse(req, async (err, fields, files) => {
+    const ext = path.extname(files.picture.filepath);
+
+    const { data, error } = await supabase.storage
+      .from("gema-product-img")
+      .upload(files.picture.newFilename, fs.createReadStream(files.picture.filepath), {
+        cacheControl: "3600",
+        upsert: false,
+        contentType: files.picture.mimetype,
+      });
+
+    try {
+      const product = await Product.findOne({ name: fields.name });
+      if (product) {
+        return res.json(409);
+      } else {
+        try {
+          const newProduct = new Product({
+            name: fields.name,
+            price: fields.price,
+            stock: fields.stock,
+            description: fields.description,
+            pictures: [files.picture.newFilename],
+            category: fields.category,
+            slug: slugify(fields.name, { lower: true }),
+          });
+          newProduct.save();
+          return res.status(200).json({ message: "El producto se guardo correctamente" });
+        } catch (error) {
+          return res.status(500).json({ message: "Error!" });
+        }
+      }
+    } catch (error) {
+      if (error) {
+        return res.status(400).json({ message: "Algún casillero esta vacío" });
       }
     }
-  } catch (error) {
-    if (error) {
-      return res.status(400).json({ message: "Algún casillero esta vacío" });
-    }
-  }
-
+  });
 }
 
 async function update(req, res) {
@@ -86,17 +107,26 @@ async function update(req, res) {
       return res.json(409);
     } else {
       try {
-        await Product.findOneAndUpdate({ slug: req.params.slug }, {
-          name: req.body.product.name,
-          category: req.body.product.category,
-          price: req.body.product.price,
-          stock: req.body.product.stock,
-          description: req.body.product.description,
-        });
+        await Product.findOneAndUpdate(
+          { slug: req.params.slug },
+          {
+            name: req.body.product.name,
+            category: req.body.product.category,
+            price: req.body.product.price,
+            stock: req.body.product.stock,
+            description: req.body.product.description,
+          },
+        );
         const updateProduct = await Product.findOne({ slug: req.params.slug });
         if (updateProduct.category !== req.body.originalCategory) {
-          await Category.findOneAndUpdate({ name: updateProduct.category }, { $push: { products: updateProduct._id } });
-          await Category.findOneAndUpdate({ name: req.body.originalCategory }, { $pull: { products: updateProduct._id } });
+          await Category.findOneAndUpdate(
+            { name: updateProduct.category },
+            { $push: { products: updateProduct._id } },
+          );
+          await Category.findOneAndUpdate(
+            { name: req.body.originalCategory },
+            { $pull: { products: updateProduct._id } },
+          );
         }
         return res.json(200);
       } catch (error) {
